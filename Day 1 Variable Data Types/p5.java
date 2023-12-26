7e0460745cb1859829c0f5f19da6f6014eac1757
Mohammad Azharuddin <95431771+ahmedazhar262@users.noreply.github.com>
2023-12-26 20:21:44 +0000

    minor changes

---
+++ Day 1 Variable Data Types/p5.java
@@ -0,0 +1,14 @@
+//  (Advanced) Will the following statement give any error in Java?
+//  int $ = 24;
+
+
+
+public class p5 {
+    public static void main(String[] args) {
+        
+        int $ = 24;
+
+        System.out.println($);
+    }
+    
+}
